c3935d993516ab8e5ba9e5e565540c32bdfd08e3
Ronald Portier <ronald@openeyedev.eu>
2026-09-08 09:23:22 -0400

    [IMP] Analyse moods and return formatted result

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 """Final project for Coursera AI based web application development."""
+import json
 import requests
 
 URL = (
@@ -12,4 +13,17 @@ def emotion_detector(text_to_analyze):
     """Analyze emotions in the text passed."""
     input_json = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(URL, json=input_json, headers=HEADERS)
-    return response.text
+    formatted_response = json.loads(response.text)
+    result = {}
+    max_mood_score = 0.0
+    dominant_mood = None
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    mood_names = ("anger", "fear", "disgust", "sadness", "joy")
+    for mood_name in mood_names:
+        mood_score = emotions[mood_name]
+        if mood_score > max_mood_score:
+            max_mood_score = mood_score
+            dominant_mood = mood_name
+        result[mood_name] = mood_score
+    result["dominant_emotion"] = dominant_mood
+    return result
