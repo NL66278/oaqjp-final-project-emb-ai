6f83a86a990196b812767b68c5f50b42d378045f
Ronald Portier <ronald@openeyedev.eu>
2026-09-08 08:15:37 -0400

    [ADD] Add emotion detector function

---
+++ emotion_detection.py
@@ -0,0 +1,15 @@
+"""Final project for Coursera AI based web application development."""
+import requests
+
+URL = (
+    "https://sn-watson-emotion.labs.skills.network"
+    "/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+)
+HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+
+def emotion_detector(text_to_analyze):
+    """Analyze emotions in the text passed."""
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(URL, json=input_json, headers=HEADERS)
+    return response.text
